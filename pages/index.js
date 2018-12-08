968e322d0b56aaa69f4b91f9518f0b482112ec6e
derekacosta <da606@georgetown.edu>
2018-12-08 16:16:05 -0500

    current working version

--- pages/index.js
+++ pages/index.js
@@ -5,26 +5,43 @@ import {config} from 'config';
 import PageLink from '../components/common/PageLink';
 import Clarifai from 'clarifai';
 import {isNullOrUndefined} from 'util';
+import _ from 'underscore';
 
-const app = new Clarifai.App({apiKey: '22cc6a81184f43dc98350f1b97907e6c'});
+const app = new Clarifai.App({apiKey: '4f69d7da58aa458baa84be2c3a56aff5'});
 
 let image = 'https://picsum.photos/500/500?image=' + Math.floor(Math.random() * Math.floor(1000)).toString();
 
-let concepts;
-
-app
-    .models
-    .initModel({id: Clarifai.GENERAL_MODEL, version: "aa7f35c01e0642fda5cf400f543e7c40"})
-    .then(generalModel => {
-        return generalModel.predict(image);
-    })
-    .then(response => {
-        console.log(response['outputs'][0]['data']['concepts']);
-    })
-
-// concepts = concepts[0].name;
-    
 class IndexPage extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            descriptors: []
+        };
+    }
+
+    componentDidMount() {
+        this.descriptors()       
+    }
+
+    descriptors() {
+        app
+            .models
+            .initModel({id: Clarifai.GENERAL_MODEL, version: "aa7f35c01e0642fda5cf400f543e7c40"})
+            .then(generalModel => {
+                return generalModel.predict(image);
+            })
+            .then(response => {
+                let desc = [...response['outputs'][0]['data']['concepts'].map(x => x.name)]
+                this.setState({
+                    descriptors: desc,
+                })
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    }
+
     render() {
         return (
             <div style={{
@@ -34,13 +51,13 @@ class IndexPage extends Component {
                     style={{
                     display: 'inline',
                     width: '50px'
-                }}><img src={image}/></div>
+                }}><img src={image + '&blur'}/></div>
                 <div
                     style={{
                     display: 'inline',
                     width: '50px'
                 }}>
-                  <p>What kind of {concepts} is in the photo?</p>
+                    <p>What kind of {this.state.descriptors[0]} is in the photo?</p>
                 </div>
             </div>
         )
